17f04b61db00f814728a930d0cce4632a7f8edc3
ID2515 <ID2515@ID2515>
2019-08-01 16:48:30 -0300

    Casi finalizado

--- src/gestiondebicicletas/Bicicleteria.java
+++ src/gestiondebicicletas/Bicicleteria.java
@@ -2,33 +2,55 @@ package gestiondebicicletas;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author ID2515
  */
 public class Bicicleteria {
-    
-      private float ganancia;
-      private int cantidadDeVentas;
-      private List<bicicleta>Bicicletas = new ArrayList<>();
-      
-      
-      public Bicicleteria(){
-        Bicicletas=new ArrayList<>();
-      }
-      
-      
-      void adBicicleta (bicicleta nuevaBici){
+    private float ganancia;
+    private int cantidadDeVentas;
+    private List<bicicleta> Bicicletas = new ArrayList<>();
+
+    public Bicicleteria() {
+        Bicicletas = new ArrayList<>();
+    }
+
+    void adBicicleta(bicicleta nuevaBici) {
         Bicicletas.add(nuevaBici);
-      }
-      
-      void venderBicicleta (bicicleta bicicleta){
-         Bicicletas.remove(bicicleta);
-         cantidadDeVentas++;
-         ganancia=ganancia+bicicleta.getPrecio();      
-      }
-      
-      
-      
-      
+    }
+
+    void venderBicicleta(bicicleta bicicleta) {
+        Bicicletas.remove(bicicleta);
+        cantidadDeVentas++;
+        ganancia = ganancia + bicicleta.getPrecio();
+    }
+
+    public bicicleta BuscarBicicleta(String nroDeSerie) {
+        for (int i = 0; i < Bicicletas.size(); i++) {
+            if (Bicicletas.get(i).getNroDeSerie().equals(nroDeSerie)) {
+                return Bicicletas.get(i);
+
+            }
+        }
+        return null;
+
+    }
+
+    List<bicicleta> ordenarBicis() {
+        bicicleta aux;
+        boolean bandera = true;
+        for (int j = 0; j < Bicicletas.size(); j++) {
+            for (int i = 0; i < Bicicletas.size(); i++) {
+                if (Bicicletas.get(i).getAño() < Bicicletas.get(i + 1).getAño()) {
+                    aux = Bicicletas.get(i + 1);
+                    Bicicletas.set(i + 1, Bicicletas.get(i));
+                    Bicicletas.set(i, aux);
+
+                }
+            }
+        }
+        return Bicicletas;
+    }
+       
 }
--- src/gestiondebicicletas/GestionDeBicicletas.java
+++ src/gestiondebicicletas/GestionDeBicicletas.java
@@ -1,6 +1,8 @@
 
 package gestiondebicicletas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ID2515
@@ -9,7 +11,16 @@ public class GestionDeBicicletas {
 
 
     public static void main(String[] args) {
-       
+        
+       Bicicleteria nuevaBicicleteria = new Bicicleteria();
+       Scanner teclado=new Scanner(System.in);
+       String numeroSerie;
+       String modelo;
+       System.out.println("Ingrese numero de serie:");
+       numeroSerie=teclado.nextLine();
+       System.out.println("Ingrese modelo");
+       modelo = teclado.nextLine(); 
+        
     }
     
 }
